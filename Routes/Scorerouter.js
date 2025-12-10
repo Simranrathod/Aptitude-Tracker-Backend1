85cf72b2d99cf2aef3c17835d4cb1ef1c7ff6893
Simranrathod <rathodsimran6666@gmail.com>
2025-12-10 15:15:54 +0530

    increase-tests

--- Routes/Scorerouter.js
+++ Routes/Scorerouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {Scoreboard,allscore,myscore}=require("../Controller/Scorecontroller");
+const {Scoreboard,allscore,myscore,increaseTestCount}=require("../Controller/Scorecontroller");
 router.post("/submit-score",Scoreboard)
 // router.get("/all-scores", async (req, res) => {
 //   const scores = await Scoreboard.find().populate("userId", "name email");
@@ -8,6 +8,6 @@ router.post("/submit-score",Scoreboard)
 // });
 router.get("/all-scores",allscore)
 router.get("/my-scores/:userId",myscore)
-// router.put("/increase-tests/:userId",increaseTestCount)
+router.put("/increase-tests/:userId",increaseTestCount)
 
 module.exports=router;
